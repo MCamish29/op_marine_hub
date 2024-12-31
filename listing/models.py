07e778626b2878d44a7b87bd8a15f38803c5fa38
Michael Camish <mcamish29@gmail.com>
2024-12-31 16:11:54 +0000

    "add wanted model"

--- listing/models.py
+++ listing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Wanted(models.Model):
+    pirate_name = models.CharField(max_length=200, unique=True)
+    bounty = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="Marine"
+    )
