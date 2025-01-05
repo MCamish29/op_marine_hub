f0c7e2c75829f3ffcd2302717c19e35a51120483
Michael Camish <mcamish29@gmail.com>
2025-01-05 14:55:53 +0000

    "staticfiles updated"

---
+++ staticfiles/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    toastElList.forEach((toastEl) => {
+        const toast = new bootstrap.Toast(toastEl);
+        toast.show();
+    });
+});
