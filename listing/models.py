68daec1492abed4186ee1009836e173105475d67
Michael Camish <mcamish29@gmail.com>
2025-01-12 09:19:33 +0000

    "add further styling to page for better UX"

--- listing/models.py
+++ listing/models.py
@@ -8,11 +8,11 @@ STATUS = ((0, "At Large!"), (1, "Caught!"))
 class Wanted(models.Model):
     pirate_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    bounty = models.IntegerField()
+    bounty = models.BigIntegerField()
     pirate_image = CloudinaryField('image', default='images/marines.png')
     description = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="Marine"
@@ -20,5 +20,8 @@ class Wanted(models.Model):
 
     def __str__(self):
         return f" {self.pirate_name} | Bounty: {self.bounty}"
+
+    def formatted_bounty(self):
+        return f"{self.bounty:,}"
 
    
--- marinehub/urls.py
+++ marinehub/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),    
+    path('summernote/', include('django_summernote.urls')),
     path("", include("listing.urls"), name="listing-urls"),
 ]
