be40b4964fc2e8b191e63cd7f805ce9d22bb80d9
Michael Camish <mcamish29@gmail.com>
2025-01-13 17:57:57 +0000

    "Update models.py"

--- listing/models.py
+++ listing/models.py
@@ -12,8 +12,8 @@ class Wanted(models.Model):
     bounty = models.BigIntegerField()
     pirate_image = CloudinaryField('image', default='images/marines.png')
     description = models.TextField()
-    created_on = models.DateField(auto_now_add=True)
-    updated_on = models.DateField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="Marine"
